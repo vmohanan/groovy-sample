9c8f4d8abb1973b7e1a7d6a18951861c89d7dfca
Vinod Mohanan <vinod.mohanan@gmail.com>
2019-03-19 15:52:30 -0400

    Update util.groovy

--- vars/util.groovy
+++ vars/util.groovy
@@ -2,7 +2,9 @@ def getRepoName()
 {
  //return scm.getUserRemoteConfigs()[0].getUrl().tokenize(‘/’).last().split(“\\.“)[0]
  //echo scm.getUserRemoteConfigs()[0].getUrl()
- return scm.getUserRemoteConfigs()[0].getUrl().tokenize('/.')[-2]
+ //return scm.getUserRemoteConfigs()[0].getUrl().tokenize('/.')[-2]
+ echo env.GIT_URL
+ return test
 }
 
 def writeSonarPropertiesFileIfNotExists(String projectName="", String sources=".", String projectBaseDir=".", String exclusions= "") {
