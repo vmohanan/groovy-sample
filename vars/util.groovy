fdfc083c531a579dabf22d30014367cc06598920
Vinod Mohanan <vinod.mohanan@gmail.com>
2019-03-19 17:17:48 -0400

    Update util.groovy

--- vars/util.groovy
+++ vars/util.groovy
@@ -8,9 +8,9 @@ def getRepoName()
 }
 
 def writeSonarPropertiesFileIfNotExists(String projectName="", String sources=".", String projectBaseDir=".", String exclusions= "") {
-    def sonarPropertiesFile = env.WORKSPACE + "/sonar-project.properties"
+    def sonarPropertiesFile = build.environment.get("WORKSPACE") + "/sonar-project.properties"
     def sonarPropertiesString = ""
-    echo env.WORKSPACE
+    echo build.environment.get("WORKSPACE")
     if (!fileExists(sonarPropertiesFile)) {
         sources = sources == "" ? getRepoName() : sources
         projectName = projectName == "" ? getRepoName() : projectName
